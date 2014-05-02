38f5d9ce59609574f4dc8dec1fd1db447857a181
CApopsicle <yoyo420303@gmail.com>
2014-05-02 23:38:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,16 +13,16 @@ var url = "http://graph.facebook.com/2014NTUIMCAMP/photos?type=uploaded";
 http.createServer(function (req, res) {
   res.writeHeader(200, {"Content-Type": "text/html"});
   
-  var data = "<html><head></head><title>2014 IM Camp</title><br><body>";
+  var data = "<html><head></head><title>2014 IM Camp</title><br><body style = 'background-color: B4EAF4;'>";
   
-  data += '<center><h1 style="font-family:Impact;color:#660077;margin-bottom:0">2014 IM Camp</h1></center>';
+  data += '<center><h1 style="font-family:Courier New;color:F47320;margin-bottom:0">2014 IM Camp</h1></center>';
 
   request.get(url, function (err, body, response) {
 
     response = JSON.parse(response);
     var count = 0;
     response.data.forEach(function (val, idx) {
-      data += "<div id ='"+count+"'><center><img src='"+val.images[2].source+"' ></center></div><br>";
+      data += "<div id ='p"+count+"'><center><img src='"+val.images[2].source+"' ></center></div><br>";
       count++;
     });
 
@@ -34,5 +34,4 @@ http.createServer(function (req, res) {
 }).listen(port);
 
 console.log("start server port: " + port);
-
 
