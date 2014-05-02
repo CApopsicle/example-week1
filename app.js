c825327f6a9cbde5f4b4198e2095c15a5ad039fb
CApopsicle <yoyo420303@gmail.com>
2014-05-02 23:17:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,20 +8,26 @@
 var http = require("http");
 var port = 1337;
 var request = require("request");
-var url = "http://graph.facebook.com/Boo/photos?type=uploaded";
+var url = "http://graph.facebook.com/2014NTUIMCAMP/photos?type=uploaded";
 
 http.createServer(function (req, res) {
   res.writeHeader(200, {"Content-Type": "text/html"});
   
-  var data = "<html><head></head><body>"
+  var data = "<html><head></head><title>2014 IM Camp</title><br><body>";
+  
+  data += '<center><h1 style="font-family:Impact;color:#660077;margin-bottom:0">2014 IM Camp</h1></center>';
+
   request.get(url, function (err, body, response) {
 
     response = JSON.parse(response);
+    var count = 0;
     response.data.forEach(function (val, idx) {
-      data += "<img src='" + val.images[2].source + "'>";
+      data += "<div id ='p"+count+"'><center><img src='"+val.images[2].source+"' ></center></div><br>";
+      count++;
     });
+
     
-    data += "</body></html>";
+    data += "</div></body></html>";
     res.end(data);
   });
 
